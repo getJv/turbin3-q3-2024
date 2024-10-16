40c663d75541ee054fec4e7c26dd92405da699a2
Jhonatan Morais <jhonatanvinicius@gmail.com>
2024-10-16 17:52:00 -0300

    day-2: challenge metadata

--- ts/cluster1/spl_metadata.ts
+++ ts/cluster1/spl_metadata.ts
@@ -7,9 +7,10 @@ import {
     DataV2Args
 } from "@metaplex-foundation/mpl-token-metadata";
 import { createSignerFromKeypair, signerIdentity, publicKey } from "@metaplex-foundation/umi";
+import bs58 from "bs58";
 
 // Define our Mint address
-const mint = publicKey("<mint address>")
+const mint = publicKey("2nVuSfTkP1DZ9ZWnNuP6GaQrLcc9Q45dGvhNpLjVG4e9")
 
 // Create a UMI connection
 const umi = createUmi('https://api.devnet.solana.com');
@@ -20,28 +21,49 @@ umi.use(signerIdentity(createSignerFromKeypair(umi, keypair)));
 (async () => {
     try {
         // Start here
-        // let accounts: CreateMetadataAccountV3InstructionAccounts = {
-        //     ???
-        // }
+         let accounts: CreateMetadataAccountV3InstructionAccounts = {
+             mint: mint,
+             mintAuthority: signer,
+             updateAuthority: signer,
+         }
 
-        // let data: DataV2Args = {
-        //     ???
-        // }
+         let data: DataV2Args = {
+             name: "http.pizza",
+             symbol: "htp",
+             uri: "https://http.pizza/",
+             sellerFeeBasisPoints: 0,
+             creators: null,
+             collection: null,
+             uses: null,
 
-        // let args: CreateMetadataAccountV3InstructionArgs = {
-        //     ???
-        // }
+         }
 
-        // let tx = createMetadataAccountV3(
-        //     umi,
-        //     {
-        //         ...accounts,
-        //         ...args
-        //     }
-        // )
+         let args: CreateMetadataAccountV3InstructionArgs = {
+             data: data,
+             isMutable: true,
+             collectionDetails: null,
+         }
 
-        // let result = await tx.sendAndConfirm(umi);
-        // console.log(bs58.encode(result.signature));
+         let tx = createMetadataAccountV3(
+             umi,
+             {
+                 ...accounts,
+                 ...args
+             }
+         )
+
+
+        let result = await tx.sendAndConfirm(umi);
+        // 3VEG6mcbjLwrpi5MTD96tADaPrdZ99wFAxC33ifZ5sYXV82NN5UPeFQJJzmGXq6pwKVTCZ2S6bfxz5i8kd2ueTrd
+        const decodedSignature = bs58.encode(result.signature)
+        console.log(decodedSignature);
+        //https://explorer.solana.com/tx/3VEG6mcbjLwrpi5MTD96tADaPrdZ99wFAxC33ifZ5sYXV82NN5UPeFQJJzmGXq6pwKVTCZ2S6bfxz5i8kd2ueTrd?cluster=devnet
+        console.log(
+            `Success! Metadata created at https://explorer.solana.com/tx/${decodedSignature}?cluster=devnet`
+        );
+
+
+
     } catch(e) {
         console.error(`Oops, something went wrong: ${e}`)
     }
